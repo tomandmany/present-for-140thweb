c2ab57f4c418a0fd77b3be761f4c8a03148b735b
鈴木翔也 <suzukishoya@suzukishouyanoMacBook-Air.local>
2024-11-05 17:22:33 +0900

    ３年生の写真を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,14 +15,10 @@ export default async function Home() {
   return (
     <main className="flex min-h-[calc(100svh-(84px+20px))] flex-col items-center justify-center pt-[calc(84px+20px)]">
       <div className="flex flex-col gap-8">
-        <Post user_name="mahohonk" user_icon="maho" posted_place="食堂2階" posted_time="19:12:00" post_base_image="maho-01" post_inner_image="maho-02" />
-        {/* <Post user_name="mahohonk" user_icon="maho" posted_place="食堂2階" posted_time="19:12:00" post_base_image="maho-02" /> */}
-        <Post user_name="onichan" user_icon="oni" posted_place="食堂2階" posted_time="19:12:00" post_base_image="oni-01" post_inner_image="oni-02" />
-        {/* <Post user_name="onichan" user_icon="oni" posted_place="食堂2階" posted_time="19:12:00" post_base_image="oni-02" /> */}
-        <Post user_name="rin0310" user_icon="rin" posted_place="食堂2階" posted_time="19:12:00" post_base_image="rin-01" post_inner_image="rin-02" />
-        {/* <Post user_name="rin0310" user_icon="rin" posted_place="食堂2階" posted_time="19:12:00" post_base_image="rin-02" /> */}
-        <Post user_name="sasisushoiya" user_icon="shoya" posted_place="食堂2階" posted_time="19:12:00" post_base_image="shoya-01" post_inner_image="shoya-02" />
-        {/* <Post user_name="sasisushoiya" user_icon="shoya" posted_place="食堂2階" posted_time="19:12:00" post_base_image="shoya-02" /> */}
+        <Post user_name="mahohonk" user_icon="maho" posted_place="食堂2階" posted_time="19:12:00" post_base_image="maho_01" post_inner_image="maho_02" />
+        <Post user_name="onichan" user_icon="oni" posted_place="食堂2階" posted_time="19:12:00" post_base_image="oni_01" post_inner_image="oni_02" />
+        <Post user_name="rin0310" user_icon="rin" posted_place="食堂2階" posted_time="19:12:00" post_base_image="rin_01" post_inner_image="rin_02" />
+        <Post user_name="sasisushoiya" user_icon="shoya" posted_place="食堂2階" posted_time="19:12:00" post_base_image="shoya_01" post_inner_image="shoya_02" />
       </div>
     </main>
   );
--- src/components/post.tsx
+++ src/components/post.tsx
@@ -15,14 +15,13 @@ interface PostProps {
 export default function Post({ user_name, user_icon, posted_place, posted_time, post_base_image, post_inner_image }: PostProps) {
     const [isBaseImage, setIsBaseImage] = useState(true);
 
-    const toggleImage = () => {
+    const toggleBaseImage = () => {
         setIsBaseImage(!isBaseImage);
     };
 
     return (
         <div className="w-[370px]">
             <div className="flex items-center gap-3 w-full mb-2 px-4">
-                {/* <div className="rounded-full w-8 aspect-square bg-gray-400" /> */}
                 <img
                     src={`/icons/${user_icon}.jpg`}
                     alt="ユーザーアイコン"
@@ -40,23 +39,21 @@ export default function Post({ user_name, user_icon, posted_place, posted_time, 
                 </div>
                 <Ellipsis className="ml-auto" />
             </div>
-            {/* <div className="rounded-xl bg-white w-full h-[520px]" /> */}
             <div className="relative">
                 <img
                     src={isBaseImage ? `/posts/${post_base_image}.jpg` : `/posts/${post_inner_image}.jpg`}
                     alt="投稿画像"
                     width={370}
                     height={520}
-                    className="rounded-xl w-full h-[520px] object-cover cursor-pointer"
-                    onClick={toggleImage}
+                    className="rounded-xl w-full h-[520px] object-cover"
                 />
                 <img
-                    src={`/posts/${post_inner_image}.jpg`}
+                    src={isBaseImage ? `/posts/${post_inner_image}.jpg` : `/posts/${post_base_image}.jpg`}
                     alt="投稿画像"
                     width={370}
                     height={520}
                     className="rounded-xl w-[110px] h-[150px] object-cover absolute top-2 left-2 border-2 border-black cursor-pointer"
-                    onClick={toggleImage}
+                    onClick={() => setIsBaseImage(!isBaseImage)}
                 />
             </div>
         </div>
